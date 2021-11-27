ec7aa47166bd6ca0c35b3d9789e91feba24f84e2
Anukarsh <anukarshr@gmail.com>
2021-11-27 19:20:02 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("This is snake game")
+console.log("This is snake game");
+
+let direction = { x: 0, y: 0 };
+let foodSound = new Audio('food.mp3')
+let gameOverSound = new Audio('gameOver.mp3')
+let moveSound = new Audio('move.mp3')
+let bgSound = new Audio('bg.mp3')
