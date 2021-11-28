4e05b7a16e9b35da31071c0b247c626dfcca10cc
Anukarsh <anukarshr@gmail.com>
2021-11-28 19:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,23 @@ const foodSound = new Audio('food.mp3')
 const gameOverSound = new Audio('gameOver.mp3')
 const moveSound = new Audio('move.mp3')
 const bgSound = new Audio('bg.mp3')
+let speed = 2;
+let lastPaintTime = 0;
 
 // Game Functions
 function main(ctime) { //ctime - current time
     window.requestAnimationFrame(main); // by this way we create a gameloop, and the function will be called repeatedly.
-    
     console.log(ctime)
+    if((ctime-lastPaintTime)/1000 < 1/speed){
+        return;
+    }
+    lastPaintTime = ctime;
+    gameEngine()
+}
 
+function gameEngine(){
+    // Part 1: Updating the snake array
+    // Part 2: 
 }
 
 
