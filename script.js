1104d41e42c212c3b7c26de301824e6ae1c3ed4f
Anukarsh <anukarshr@gmail.com>
2021-12-08 11:33:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ const bgSound = new Audio("bg.mp3");
 let speed = 2;
 let lastPaintTime = 0;
 let snakeArr = [{ x: 13, y: 15 }];
-food = {x:18, y:13}
+food = { x: 18, y: 13 };
 
 // Game Functions
 function main(ctime) {
   //ctime - current time
   window.requestAnimationFrame(main); // by this way we create a gameloop, and the function will be called repeatedly.
-  console.log(ctime);
+  // console.log(ctime);
   if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return;
   }
@@ -24,8 +24,29 @@ function main(ctime) {
   gameEngine();
 }
 
+function isCollide(snakeArr) {
+  return false;
+}
+
 function gameEngine() {
   // Part 1: Updating the snake array & food
+  if (isCollide(snakeArr)) {
+    gameOverSound.play();
+    bgSound.pause();
+    inputDir = { x: 0, y: 0 };
+    alert("Game over, press any key to restart");
+    snakeArr = [{ x: 13, y: 15 }];
+    bgSound.play();
+    score = 0;
+  }
+
+  // If food has been eaten, increase the score and regen the food
+  if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    snakeArr.unshift({
+      x: snakeArr[0].x + inputDir.x,
+      y: snakeArr[0].y + inputDir.y,
+    });
+  }
 
   // Part 2: Display the snake and it's food
   //   Display the snake
@@ -35,11 +56,10 @@ function gameEngine() {
     snakeElement.style.gridRowStart = e.y;
     snakeElement.style.gridColumnStart = e.x;
     snakeElement.classList.add("s-body");
-    if(index === 0){
-        snakeElement.classList.add("s-head");
-    }
-    else{
-        snakeElement.classList.add("s-body");
+    if (index === 0) {
+      snakeElement.classList.add("s-head");
+    } else {
+      snakeElement.classList.add("s-body");
     }
     board.appendChild(snakeElement);
   });
@@ -49,12 +69,34 @@ function gameEngine() {
   foodElement.style.gridColumnStart = food.x;
   foodElement.classList.add("food");
   board.appendChild(foodElement);
-
 }
 
 // Main logic starts here
 window.requestAnimationFrame(main);
 
-window.addEventListener('keydown', e=>{
-    
-})
+window.addEventListener("keydown", (e) => {
+  inputDir = { x: 0, y: 1 }; // start the game
+  moveSound.play();
+  switch (e.key) {
+    case "ArrowUp":
+      console.log("ArrowUp");
+      inputDir.x = 0;
+      inputDir.y = -1;
+      break;
+    case "ArrowDown":
+      console.log("ArrowDown");
+      inputDir.x = 0;
+      inputDir.y = 1;
+      break;
+    case "ArrowLeft":
+      console.log("ArrowLeft");
+      inputDir.x = -1;
+      inputDir.y = 0;
+      break;
+    case "ArrowRight":
+      console.log("ArrowRight");
+      inputDir.x = 1;
+      inputDir.y = 0;
+      break;
+  }
+});
